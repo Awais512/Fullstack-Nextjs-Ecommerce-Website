54f0b5f79d76660465c3fcb2bc5ea1a1dfce7488
awais512 <raza.awais25@gmail.com>
2023-01-03 15:22:26 +0000

    Sign in page 3... Custom input with yup

--- pages/signin.js
+++ pages/signin.js
@@ -1,14 +1,38 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styles from "../styles/signin.module.scss";
 import { BiLeftArrowAlt } from "react-icons/bi";
 import Link from "next/link";
 import { Form, Formik } from "formik";
+import * as Yup from "yup";
 import LoginInput from "../components/Inputs/LoginInput";
 
-const signin = () => {
+const initialValues = {
+  login_email: "",
+  login_password: "",
+};
+
+const Signin = () => {
+  const [user, setUser] = useState(initialValues);
+
+  const { login_email, login_password } = user;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginValidation = Yup.object({
+    login_email: Yup.string()
+      .required("Email address is Required")
+      .email("Please Enter a valid email address"),
+    login_password: Yup.string().required("Please Enter a Password"),
+  });
+
+  console.log(user);
+
   return (
     <>
       <Header />
@@ -25,10 +49,27 @@ const signin = () => {
           <div className={styles.login__form}>
             <h1>Sign in</h1>
             <p>Get access to one of the best Eshopping service in the world</p>
-            <Formik>
+            <Formik
+              enableReinitialize
+              validationSchema={loginValidation}
+              initialValues={{ login_email, login_password }}
+            >
               {(form) => (
                 <Form>
-                  <LoginInput icon="email" placeholder="Email Address" />
+                  <LoginInput
+                    type="text"
+                    name="login_email"
+                    icon="email"
+                    placeholder="Enter Your Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="login_password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
                 </Form>
               )}
             </Formik>
@@ -40,4 +81,4 @@ const signin = () => {
   );
 };
 
-export default signin;
+export default Signin;
