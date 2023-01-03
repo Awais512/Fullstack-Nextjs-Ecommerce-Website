fc2ee075c96789cc879d282e8c79294f31dd3621
awais512 <raza.awais25@gmail.com>
2023-01-03 23:34:58 +0000

    Signup form and validation with yup and formik

--- pages/signin.js
+++ pages/signin.js
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unescaped-entities */
 import React, { useState } from "react";
-import Image from "next/image";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import styles from "../styles/signin.module.scss";
@@ -11,22 +10,27 @@ import * as Yup from "yup";
 import LoginInput from "../components/Inputs/LoginInput";
 import CircledIconBtn from "../components/Buttons/CircledIconBtn";
 import { getProviders, signIn } from "next-auth/react";
-import facebook from "../public/images/facebook.png";
-import google from "../public/images/google.png";
-import auth0 from "../public/images/auth0.png";
-import github from "../public/images/github.png";
-import twitter from "../public/images/twitter (legacy).png";
 
 const initialValues = {
   login_email: "",
   login_password: "",
+  full_name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
 };
 
 const Signin = ({ providers }) => {
-  console.log(providers);
   const [user, setUser] = useState(initialValues);
 
-  const { login_email, login_password } = user;
+  const {
+    login_email,
+    login_password,
+    full_name,
+    email,
+    password,
+    confirm_password,
+  } = user;
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -38,6 +42,28 @@ const Signin = ({ providers }) => {
       .required("Email address is Required")
       .email("Please Enter a valid email address"),
     login_password: Yup.string().required("Please Enter a Password"),
+  });
+
+  const registerValidation = Yup.object({
+    full_name: Yup.string()
+      .required("What's is your name?")
+      .min(2, "First name must be 2 and 16 characters")
+      .max(16, "First name must be 2 and 16 characters")
+      .matches(/^[aA-zZ]/, "Numbers and Special characters are not allowed."),
+    email: Yup.string()
+      .required(
+        "You'll need this when you login and if you ever need to reset your password."
+      )
+      .email("Please Enter a valid email address"),
+    password: Yup.string()
+      .required(
+        "Enter a combination of at least six numbers,letters and punctuation marks(such as ! and &)"
+      )
+      .min(6, "Password must be atleast 6 characters")
+      .max(36, "Password can't be more than 36 characters"),
+    confirm_password: Yup.string()
+      .required("Confirm Your Password")
+      .oneOf([Yup.ref("password")], "Password must match"),
   });
 
   console.log(user);
@@ -102,6 +128,52 @@ const Signin = ({ providers }) => {
                 ))}
               </div>
             </div>
+          </div>
+        </div>
+        <div className={styles.login__container}>
+          <div className={styles.login__form}>
+            <h1>Sign Up</h1>
+            <p>Get access to one of the best Eshopping service in the world</p>
+            <Formik
+              enableReinitialize
+              validationSchema={registerValidation}
+              initialValues={{ email, full_name, password, confirm_password }}
+            >
+              {(form) => (
+                <Form>
+                  <LoginInput
+                    type="text"
+                    name="full_name"
+                    icon="user"
+                    placeholder="Full Name"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="text"
+                    name="email"
+                    icon="email"
+                    placeholder="Email Address"
+                    onChange={handleChange}
+                  />
+                  <LoginInput
+                    type="password"
+                    name="password"
+                    icon="password"
+                    placeholder="Password"
+                    onChange={handleChange}
+                  />
+
+                  <LoginInput
+                    type="password"
+                    name="confirm_password"
+                    icon="password"
+                    placeholder="Re-type Password"
+                    onChange={handleChange}
+                  />
+                  <CircledIconBtn type="submit" text="Sign Up" />
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
       </div>
