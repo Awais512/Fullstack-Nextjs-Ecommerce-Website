ab327da93b1c0353bc27f9d8a0ec141a50daa960
awais512 <raza.awais25@gmail.com>
2023-01-09 22:13:43 +0000

    Flash Deals Responsiveness'

--- components/Home/FlashDeals/index.jsx
+++ components/Home/FlashDeals/index.jsx
@@ -19,11 +19,28 @@ const FlashDeals = () => {
         <Countdown />
       </div>
       <Swiper
-        slidesPerView={6}
-        spaceBetween={20}
+        slidesPerView={1}
+        spaceBetween={10}
         navigation={true}
         modules={[Navigation]}
         className="flashDeals__swiper"
+        breakpoints={{
+          450: {
+            slidesPerView: 2,
+          },
+          630: {
+            slidesPerView: 3,
+          },
+          920: {
+            slidesPerView: 4,
+          },
+          1232: {
+            slidesPerView: 5,
+          },
+          1520: {
+            slidesPerView: 6,
+          },
+        }}
       >
         <div className={styles.flashDeals__list}>
           {flashDealsArray.map((item, i) => (
