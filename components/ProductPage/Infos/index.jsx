1774e3bfbf28778f3d0fcf9ccaecc882e7e3b799
awais512 <raza.awais25@gmail.com>
2023-01-28 14:43:57 +0000

    Details page infos 2

--- components/ProductPage/Infos/index.jsx
+++ components/ProductPage/Infos/index.jsx
@@ -1,7 +1,15 @@
 import styles from "./styles.module.scss";
 import { Rating } from "@mui/material";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import Link from "next/link";
 
 const Infos = ({ product }) => {
+  console.log(product);
+  const router = useRouter();
+
+  const [size, setSize] = useState(router.query.size);
+
   return (
     <div className={styles.infos}>
       <div className={styles.infos__container}>
@@ -19,19 +27,46 @@ const Infos = ({ product }) => {
           {product.numReviews == 1 ? "review" : "reviews"}
         </div>
         <div className={styles.infos__price}>
-          {product.priceRange ? (
-            <h2>{product.priceRange}</h2>
-          ) : (
-            <h1>{product.price}</h1>
-          )}
+          {!size ? <h2>{product.priceRange}</h2> : <h1>{product.price}</h1>}
           {product.discount > 0 ? (
             <h3>
-              <span>{product.priceBefore}</span>
+              {size && <span>{product.priceBefore}</span>}
               <span>(-{product.discount}%)</span>
             </h3>
           ) : (
             ""
           )}
+        </div>
+        <span className={styles.infos__shipping}>
+          {product.shipping
+            ? `+$${product.shipping} Shipping fee`
+            : `Free Shipping`}
+        </span>
+        <span>
+          {!size
+            ? product.quantity
+            : product.sizes.reduce((start, next) => start + next.qty, 0)}{" "}
+          Pieces.
+        </span>
+        <div className={styles.infos__sizes}>
+          <h4>Select a Size :</h4>
+          <div className={styles.infos__sizes_wrap}>
+            {product.sizes.map((size, i) => (
+              <Link
+                key={i}
+                href={`/product/${product.slug}?style=${router.query.style}&size=${i}`}
+              >
+                <div
+                  className={`${styles.infos__sizes_size} ${
+                    i == router.query.size && styles.active_size
+                  }`}
+                  onClick={() => setSize(size.size)}
+                >
+                  {size.size}
+                </div>
+              </Link>
+            ))}
+          </div>
         </div>
       </div>
     </div>
--- pages/product/[slug].jsx
+++ pages/product/[slug].jsx
@@ -65,10 +65,12 @@ export async function getServerSideProps(context) {
     colors: product.subProducts.map((p) => {
       return p.color;
     }),
-    priceRange:
-      prices.length > 1
-        ? `From ${prices[0]} to ${prices[prices.length - 1]}`
-        : "",
+    priceRange: subProduct.discount
+      ? `From ${(prices[0] - prices[0] / subProduct.discount).toFixed(2)} to ${(
+          prices[prices.length - 1] -
+          prices[prices.length - 1] / subProduct.discount
+        ).toFixed(2)}`
+      : `From ${prices[0]} to ${prices[prices.length - 1]}`,
     price:
       subProduct.discount > 0
         ? (
